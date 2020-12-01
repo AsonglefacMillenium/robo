17af704877f8753a53c83dbddec3408a8ecb407f
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-12-01 16:36:22 +0100

    Added background color and styled searchbox

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -4,11 +4,11 @@ const Searchbox = ({
     searchfield,
     searchChange
 }) => {
-    return ( < div className = '' >
+    return ( < div className = 'ma5' >
         <
         input type = 'search'
         placeholder = 'Search Friends'
-        className = 'bg-light-blue'
+        className = 'bg-light-blue pa3 br3 f4 grow'
         onChange = { searchChange }
         / > < /
         div >
