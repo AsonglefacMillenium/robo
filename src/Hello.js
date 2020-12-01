5b01c7f6516cd0df669b0778c55f2b00ef0b7390
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-12-01 01:22:30 +0100

    Dealing with props and tachyon classes

--- src/Hello.js
+++ src/Hello.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 import './Hello.css'
 
-class Hello extends Component {
-    render() {
-        return ( < div >
-            <
-            h1 > Welcome to < /h1>  <
-            p > { this.props.greeting } < /p>    < /
-            div >
-        );
-    }
+const Hello = (props) => {
+
+    return ( < div className = "f1 tc bg-blue" >
+        <
+        h1 > Welcome to < /h1>  <
+        p > { props.greeting } < /p>    < /
+        div >
+    );
+
 }
 
 export default Hello;
